4fc0c6bfc8e0874fbabb36de7921782dbd45c037
kostya-lee003 <72404363+kostya-lee003@users.noreply.github.com>
2020-10-07 12:26:26 +0500

    Add files via upload
    this is square

---
+++ Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+     public static void main(String[] args) {
+          for (int i = 0; i < 3; i++) {
+               System.out.print("00");
+               System.out.println("0");
+          }
+     }
+}
+
+
+
+
